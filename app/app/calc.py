03a9fb0a4d8350081d9907d86fab27800e306585
Adel Mohamadzade <adel.mohamadzadeph@gmail.com>
2022-06-27 09:59:17 +0430

    Add simple tests.

---
+++ app/app/calc.py
@@ -0,0 +1,19 @@
+"""
+Calculator functions
+"""
+
+def add(x: int, y: int) -> int:
+    """Add x and y and return result.
+
+    :param x: first number
+    :type x: int
+    :param y: second number
+    :type y: int
+    :return: sum of two numbers
+    :rtype: int
+    """
+    return x + y
+
+def subtract(x: int, y: int) -> int:
+    """Subtract to numbers"""
+    return abs(x - y)
